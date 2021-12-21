39fcb419b13e632b555c7ea7e8c40db2a3fe4848
muttta <93360763+muttta@users.noreply.github.com>
2021-12-21 18:04:50 +0900

    不要モデルの削除

--- db/migrate/20211221072329_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :users do |t|
-      t.date   :birthday,      null: false
-      t.string :nickname,      null:false
-      t.timestamps
-    end
-  end
-end
