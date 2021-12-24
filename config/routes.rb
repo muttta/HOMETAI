0f52e292a70c5d3b539a0505eebf42a91d6bdc5c
muttta <93360763+muttta@users.noreply.github.com>
2021-12-24 19:10:52 +0900

    コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   root to:"diaries#index"
   post '/diaries/result', to: 'diaries#result'
-  resources :diaries, only:[:index, :new, :create, :save]
+  resources :diaries, only:[:index, :new, :create]
+  resources :calendars, only:[:index, :new, :create]
 end
